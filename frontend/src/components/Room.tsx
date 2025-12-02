4178368d9b340c913243c00f82021fe78a9182c3
jttw1953 <tung.ti@northeastern.edu>
2025-12-01 18:39:11 -0800

    fixed ICE servers

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -440,12 +440,11 @@ export const Room = ({
 
         const pc = new RTCPeerConnection({
             iceServers: [
-                {
-                    urls: [
-                        'stun:stun.l.google.com:19302',
-                        'stun:stun1.l.google.com:19302',
-                    ]
-                }
+                { urls: 'stun:stun.l.google.com:19302' },
+                { urls: 'stun:stun1.l.google.com:19302' },
+                { urls: 'stun:stun2.l.google.com:19302' },
+                { urls: 'stun:stun3.l.google.com:19302' },
+                { urls: 'stun:stun4.l.google.com:19302' },
             ]
         });
 
