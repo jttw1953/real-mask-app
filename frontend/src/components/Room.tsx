144204d5573fbc7e149c18ed5c1e27b288fbc879
jttw1953 <tung.ti@northeastern.edu>
2025-12-01 18:46:02 -0800

    fixed ICE servers

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -442,12 +442,19 @@ export const Room = ({
             iceServers: [
                 { urls: 'stun:stun.l.google.com:19302' },
                 { urls: 'stun:stun1.l.google.com:19302' },
-                { urls: 'stun:stun2.l.google.com:19302' },
-                { urls: 'stun:stun3.l.google.com:19302' },
-                { urls: 'stun:stun4.l.google.com:19302' },
-            ]
+                { 
+                    urls: 'turn:openrelay.metered.ca:80',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject',
+                },
+                { 
+                    urls: 'turn:openrelay.metered.ca:443',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject',
+                },
+            ],
+            iceCandidatePoolSize: 10,
         });
-
         pcRef.current = pc;
 
         pc.ontrack = (event) => {
